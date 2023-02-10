d48443b53b51ec763a8c5fc766e83b4dd29188e9
Lev Tsypanov <levdark7@gmail.com>
2023-02-10 19:07:26 +0300

    deleted

--- dist/vk-id/UrlUtils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function makeUrl(url: string, params: any): string;
-export declare function makeParams(params: any): string;
-export declare function request(url: string, params: any): Promise<any>;
