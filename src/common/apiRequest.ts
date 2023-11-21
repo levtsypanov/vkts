8c7774d9cc5c497737307cccababdf153accad89
Lev Tsypanov <levdark7@gmail.com>
2023-11-21 14:31:03 +0300

    chore(apiRequest): сorrected an erroneous deploy

--- src/common/apiRequest.ts
+++ src/common/apiRequest.ts
@@ -1,42 +1,30 @@
-import { useState, useEffect } from 'react';
-
 export let requestConfig: { apiVersion?: string, langId?: number | string, apiUrl?: string, access_token?: string } = {};
 
-export function apiRequest(apiMethod: string, requestData: object = {} ) {
-  const [data, setData] = useState<any>([]);
-  const [loading, setLoading] = useState(true);
-
-  const buildUrl = () => {
-    const queryParams: string[] = [];
-    // Добавление параметра access_token
-    if (requestConfig.access_token) {
-      queryParams.push(`access_token=${requestConfig.access_token}`);
-    }
-    // Добавление параметра langId
-    if (requestConfig.langId) {
-      queryParams.push(`langId=${requestConfig.langId}`);
-    }
-    // Добавление дополнительных параметров из requestData
-    Object.entries(requestData).forEach(([key, value]) => {
-      queryParams.push(`${key}=${encodeURIComponent(value)}`);
-    });
-
-    const queryString = queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
-    return `${requestConfig.apiUrl}/method/${apiMethod}${queryString}`;
-  };
-
-  async function fetchUrl() {
-    const url = buildUrl();
-    const response = await fetch(url);
-    const json = await response.json();
-    setData(json);
-    setLoading(false);
+async function apiRequest(apiMethod: string, requestData: object = {}) {
+  const queryParams: string[] = [];
+  // Добавление параметра access_token
+  if (requestConfig.access_token) {
+    queryParams.push(`access_token=${requestConfig.access_token}`);
   }
+  // Добавление параметра langId
+  if (requestConfig.langId) {
+    queryParams.push(`langId=${requestConfig.langId}`);
+  }
+  // Добавление параметра apiVersion
+  if (requestConfig.apiVersion) {
+    queryParams.push(`v=${requestConfig.apiVersion}`);
+  }
+  // Добавление дополнительных параметров из requestData
+  Object.entries(requestData).forEach(([key, value]) => {
+    queryParams.push(`${key}=${encodeURIComponent(value)}`);
+  });
 
-  useEffect(() => {
-    fetchUrl();
-    console.clear();
-  }, []);
+  const queryString = queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
+  const url = `${requestConfig.apiUrl}/method/${apiMethod}${queryString}`;
 
-  return [data, loading];
-};
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+export { apiRequest };
